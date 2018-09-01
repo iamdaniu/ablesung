0096f9c19eaf72e494c9ad2f29731532bac3d0b6
unknown <joernspam@gmx.de>
2018-09-01 09:16:52 +0200

    initial version

---
+++ src/main/java/de/daniu/home/ablesung/AblesungApplication.java
@@ -0,0 +1,12 @@
+package de.daniu.home.ablesung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AblesungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AblesungApplication.class, args);
+	}
+}
