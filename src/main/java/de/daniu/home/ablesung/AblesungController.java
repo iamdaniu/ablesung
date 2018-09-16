5848675a4accf804c5f6710f835de63efe4bcdee
iamdaniu <joernspam@gmx.de>
2018-09-16 15:20:48 +0200

    adding thymeleaf

--- src/main/java/de/daniu/home/ablesung/AblesungController.java
+++ src/main/java/de/daniu/home/ablesung/AblesungController.java
@@ -1,6 +1,7 @@
 package de.daniu.home.ablesung;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -11,8 +12,10 @@ import java.net.URI;
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
+
 @Controller
-@RequestMapping(value = "/ablesungen")
+@RequestMapping(value = "/")
 public class AblesungController {
     private static final LocalDate DEFAULT_VON_DATE = LocalDate.of(1999, 1, 1);
     private static final LocalDate DEFAULT_BIS_DATE = LocalDate.of(3000, 2, 1);
@@ -21,8 +24,8 @@ public class AblesungController {
     private AblesungService service;
 
     @GetMapping
-    public List<Ablesung> getAblesungen(@RequestParam LocalDate von,
-                                        @RequestParam LocalDate bis) {
+    public List<Ablesung> getAblesungen(@RequestParam @DateTimeFormat(iso = DATE) LocalDate von,
+                                        @RequestParam @DateTimeFormat(iso = DATE) LocalDate bis) {
         von = von != null ? von : DEFAULT_VON_DATE;
         bis = bis != null ? bis : DEFAULT_BIS_DATE;
         return service.getAblesungen(von, bis);
@@ -30,16 +33,21 @@ public class AblesungController {
 
     @GetMapping(value = "/meter/{meterId}")
     public List<Ablesung> getAblesungen(@PathVariable("meterId") String meterId,
-                                        @RequestParam LocalDate von,
-                                        @RequestParam LocalDate bis) {
+                                        @RequestParam @DateTimeFormat(iso = DATE) LocalDate von,
+                                        @RequestParam @DateTimeFormat(iso = DATE) LocalDate bis) {
         von = von != null ? von : DEFAULT_VON_DATE;
         bis = bis != null ? bis : DEFAULT_BIS_DATE;
         return service.getAblesungen(meterId, von, bis);
     }
 
+    @GetMapping(value="/readingsList")
+    public List<Ablesung> getAblesungen() {
+        return service.getAblesungen(LocalDate.MIN, LocalDate.MAX);
+    }
+
     @PostMapping(value = "/meter/{meterId}")
     public ResponseEntity<Ablesung> addAblesung(@PathVariable("meterId") String meterId,
-                                                @RequestParam LocalDate datum,
+                                                @RequestParam @DateTimeFormat(iso = DATE) LocalDate datum,
                                                 @RequestParam BigDecimal wert) {
         Ablesung ablesung = Ablesung.SimpleAblesung.builder()
                 .wert(wert)
